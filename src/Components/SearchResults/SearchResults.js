1c254ecdf19261459be1c44dfb2a480fb8233c90
abel <abel_perez9@hotmail.com>
2023-06-20 11:42:28 -0500

    added components

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SearchResults.css";
+
+export default function SearchResults() {
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+      {/* Add a Tracklist Component */}
+    </div>
+  );
+}
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./TrackList.css";
+
+export function TrackList() {
+  return (
+    <div className="TrackList">
+      {/* Return a map method that renders a set of Track Components */}
+    </div>
+  );
+}
